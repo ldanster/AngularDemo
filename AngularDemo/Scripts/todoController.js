ba0fd0c95b6a6a6c487600c5fa6776e0c3ed1a7d
Lebohang <lebohangdanster@gmail.com>
2015-04-09 15:15:01 +0200

    Add functionality to show the items

--- AngularDemo/Scripts/todoController.js
+++ AngularDemo/Scripts/todoController.js
@@ -3,8 +3,16 @@
 angular.module("todoApp", [])
 .controller("todoController", function($scope, $http) {
 
+    $scope.todoItem = {
+        Name: "Lebohang",
+        IsComplete: "False"
+    }
+
     $scope.getAllItems = function() {
-        
+        $http.get("/api/todo")
+        .success(function(data) {
+            $scope.todoItems = data;
+        });
     }
 
     $scope.getItem = function(id) {
@@ -19,4 +27,5 @@ angular.module("todoApp", [])
         
     }
 
+    $scope.getAllItems();
 });
