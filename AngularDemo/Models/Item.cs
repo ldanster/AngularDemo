6efd449ca5733d814326b7f82e72fe9c21e61e6f
Lebohang <lebohangdanster@gmail.com>
2015-04-09 13:52:39 +0200

    Initial commit

---
+++ AngularDemo/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngularDemo.Models
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
